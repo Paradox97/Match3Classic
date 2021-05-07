2b0c0f293c82c06c85586f2a086cce023ee4587d
ILYA <morozov.il.vic@gmail.com>
2021-05-07 17:06:58 +1000

    separating logic
    start of major changes
    new classes Texture, singleton MousePosition(to be transfered to MouseInput)

---
+++ Controls/MousePosition.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Match3Classic.Controls
+{
+    class MousePosition
+    {
+        private static MousePosition input;
+
+        MouseState mouseInput;
+        public MousePosition()
+        {
+           this.mouseInput = Mouse.GetState();
+        }
+
+        public static MousePosition getMouseInput()
+        {
+            if (input == null)
+                input = new MousePosition();
+            return input;
+        }
+
+    }
+}
---
+++ ScreenEntities/Texture.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Match3Classic.ScreenEntities
+{
+    class Texture
+    {
+        Texture2D[] textureset;
+        Texture2D currentTexture;
+        int state, maxstates,
+            stateByTime = 15;
+        const int SPECIALSTATES = 2;
+
+        public Texture(Texture2D[] _textureset)
+        {
+            this.textureset = _textureset;
+            this.maxstates = _textureset.Length - SPECIALSTATES;
+            this.state = 0;
+            this.currentTexture = this.textureset[state];
+        }
+
+        public void ChangeState()
+        {
+            this.currentTexture = textureset[(state / stateByTime) % maxstates];
+        }
+
+        public Texture2D GetTexture()
+        {
+            return this.currentTexture;
+        }
+    }
+}
